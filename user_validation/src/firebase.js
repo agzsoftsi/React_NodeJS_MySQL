a1df64fda632a17bf6b9d560f1c9fbd123ea9fe2
agzsoftsi <agzsoftsi@gmail.com>
2020-04-16 17:55:59 -0500

    Create firebase.js

---
+++ user_validation/src/firebase.js
@@ -0,0 +1,10 @@
+export default{
+    apiKey: "AIzaSyDAuya564H0K5dk7laPjPhjJlUyiBiXSr8",
+    authDomain: "userdb-1.firebaseapp.com",
+    databaseURL: "https://userdb-1.firebaseio.com",
+    projectId: "userdb-1",
+    storageBucket: "userdb-1.appspot.com",
+    messagingSenderId: "740215375027",
+    appId: "1:740215375027:web:e3f1612329cc0d91315cc6",
+    measurementId: "G-0RLVTPHNES"
+}
